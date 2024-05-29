1cbaa11663e7e92399aa1134cda5ef5b087a9fcf
Ganesh Pai <43885559+paiganesh@users.noreply.github.com>
2024-05-29 11:40:35 +0530

    Update YouTubeDownloader_main.py

--- youtube_video_downloader/YouTubeDownloader_main.py
+++ youtube_video_downloader/YouTubeDownloader_main.py
@@ -1,10 +1,10 @@
 from ytube import YouTube
 
-utube_URL = 'https://www.youtube.com/playlist?list=PLR2yPNIFMlL9UUF6-syrVrNaRwHVJofZE'
-download_dir = "G:\\Library\\Java"
+utube_URL = 'https://www.youtube.com/playlist?list=ABCDPQR'  # specify a valid playlist URL
+download_dir = "D:\\Download_Dir"
 
-utube_URL = 'https://www.youtube.com/watch?v=TU3Vq8MkNHk'
-download_dir = "G:\\Library\\Java\\Spring Boot Tutorials - Code Java"
+utube_URL = 'https://www.youtube.com/watch?v=ABCDPQR'        # specify a valid video URL
+download_dir = "D:\\Download_Dir"
 
 
 YouTube().download_videos(utube_URL, dest_dir=download_dir, playlist_autonumber=False)
